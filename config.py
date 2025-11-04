91cfb1b2a32be5f98d05fd284a3f9af91d21035d
y7ozel <yulkahan.ozel@gmail.com>
2025-11-04 16:51:23 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# config.py
+"""
+Configuration settings for automation scripts
+"""
+# URLs
+LOGIN_URL = "https://the-internet.herokuapp.com/login"
+PRACTICE_FORM_URL = "https://demoqa.com/automation-practice-form"
+
+# Test Data
+TEST_CREDENTIALS = {
+    "username": "tomsmith",
+    "password": "SuperSecretPassword!"
+}
+
+# Wait times
+SHORT_WAIT = 5
+LONG_WAIT = 10
